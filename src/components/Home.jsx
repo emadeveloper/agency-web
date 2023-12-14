43c2f1d13e83ebff042ca8b80a34da6f1d1bcfdd
Emanuel Martinez <yinproducciones@gmail.com>
2023-12-14 14:35:06 -0300

    feat:home, fininsh home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,62 @@ const Home = () => {
     <div className="bg-neutralSilver">
       <div className="px-4 lg:px-14 max-w-screen-2x1 mx-auto min-h-screen h-screen">
         <Carousel className="w-full mx-auto">
-          <div className="my-28 md:my-8 py-12  flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          {/* hero image */}
+         {/*  <div className="my-28 md:my-8 py-12  flex flex-col md:flex-row-reverse items-center justify-between gap-12">
             <img src="" alt="" />
           </div>
-          <div className="flex h-full items-center justify-center">
-            Slide 2
+          {/* hero text */}
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+              Lessons and insights{" "}
+              <span className="text-brandPrimary leading-snug">
+                {" "}
+                from 8 years
+              </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8">
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+            <button className="btn-primary">Register</button>
           </div>
-          <div className="flex h-full items-center justify-center">
-            Slide 3
+          {/* hero image */}
+        {/*   <div className="my-28 md:my-8 py-12  flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+            <img src="" alt="" />
+          </div> */}
+          {/* hero text */}
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+              Learn and Earn Money {" "}
+              <span className="text-brandPrimary leading-snug">
+                {" "}
+                in 4 Months
+              </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8">
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+            <button className="btn-primary">Register</button>
+          </div>
+          {/* hero image */}
+          {/* <div className="my-28 md:my-8 py-12  flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+            <img src="" alt="" />
+          </div> */}
+          {/* hero text */}
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+              Lessons and insights{" "}
+              <span className="text-brandPrimary leading-snug">
+                {" "}
+                from 8 years
+              </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8">
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+            <button className="btn-primary">Register</button>
           </div>
         </Carousel>
       </div>
